d0ced1affa9b3523dd703de149245ff17b8e4d3f
dan-kwiat <5648607+dan-kwiat@users.noreply.github.com>
2024-01-17 11:41:40 +0000

    feat(styles): use pink theme

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -121,16 +121,16 @@ export default function Page({ children }) {
                                   href={item.href}
                                   className={classNames(
                                     item.current
-                                      ? "bg-gray-50 text-indigo-600"
-                                      : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                                      ? "bg-gray-50 text-pink-600"
+                                      : "text-gray-700 hover:text-pink-600 hover:bg-gray-50",
                                     "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                                   )}
                                 >
                                   <item.icon
                                     className={classNames(
                                       item.current
-                                        ? "text-indigo-600"
-                                        : "text-gray-400 group-hover:text-indigo-600",
+                                        ? "text-pink-600"
+                                        : "text-gray-400 group-hover:text-pink-600",
                                       "h-6 w-6 shrink-0"
                                     )}
                                     aria-hidden="true"
@@ -154,16 +154,16 @@ export default function Page({ children }) {
                                   href={item.href}
                                   className={classNames(
                                     item.current
-                                      ? "bg-gray-50 text-indigo-600"
-                                      : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                                      ? "bg-gray-50 text-pink-600"
+                                      : "text-gray-700 hover:text-pink-600 hover:bg-gray-50",
                                     "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                                   )}
                                 >
                                   <item.icon
                                     className={classNames(
                                       item.current
-                                        ? "text-indigo-600"
-                                        : "text-gray-400 group-hover:text-indigo-600",
+                                        ? "text-pink-600"
+                                        : "text-gray-400 group-hover:text-pink-600",
                                       "h-6 w-6 shrink-0"
                                     )}
                                     aria-hidden="true"
@@ -209,16 +209,16 @@ export default function Page({ children }) {
                           href={item.href}
                           className={classNames(
                             item.current
-                              ? "bg-gray-50 text-indigo-600"
-                              : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                              ? "bg-gray-50 text-pink-600"
+                              : "text-gray-700 hover:text-pink-600 hover:bg-gray-50",
                             "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                           )}
                         >
                           <item.icon
                             className={classNames(
                               item.current
-                                ? "text-indigo-600"
-                                : "text-gray-400 group-hover:text-indigo-600",
+                                ? "text-pink-600"
+                                : "text-gray-400 group-hover:text-pink-600",
                               "h-6 w-6 shrink-0"
                             )}
                             aria-hidden="true"
@@ -242,16 +242,16 @@ export default function Page({ children }) {
                           href={item.href}
                           className={classNames(
                             item.current
-                              ? "bg-gray-50 text-indigo-600"
-                              : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                              ? "bg-gray-50 text-pink-600"
+                              : "text-gray-700 hover:text-pink-600 hover:bg-gray-50",
                             "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                           )}
                         >
                           <item.icon
                             className={classNames(
                               item.current
-                                ? "text-indigo-600"
-                                : "text-gray-400 group-hover:text-indigo-600",
+                                ? "text-pink-600"
+                                : "text-gray-400 group-hover:text-pink-600",
                               "h-6 w-6 shrink-0"
                             )}
                             aria-hidden="true"
